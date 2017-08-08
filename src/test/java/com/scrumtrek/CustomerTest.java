70e8d01c3c0f700076c80e2117dd1075280538c7
Gleyzerman <evgley@gmail.com>
2017-08-08 13:40:21 +0300

    initial

--- src/test/java/com/scrumtrek/CustomerTest.java
+++ src/test/java/com/scrumtrek/CustomerTest.java
@@ -2,6 +2,7 @@ package com.scrumtrek;
 
 import com.scrumtrek.simplestore.Customer;
 import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
 import com.scrumtrek.simplestore.Rental;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
@@ -32,38 +33,47 @@ public class CustomerTest {
         System.out.println(c.Statement());
     }
 
+    public Movie CreateMovie(String title, PriceCodes priceCode)
+    {
+        Movie m = mock(Movie.class);
+        when(m.getPriceCode())
+                .thenReturn(priceCode);
+        when(m.getTitle())
+                .thenReturn(title);
+
+        return m;
+    }
+
+    public Rental CreateRentalStub(Movie m, int daysRented)
+    {
+        Rental r = mock(Rental.class);
+
+        when(r.getDaysRented())
+                .thenReturn(daysRented);
+        when(r.getMovie())
+                .thenReturn(m);
+
+        return r;
+    }
+
     @Test
     public void addRental(){
         Customer c = new Customer("test");
 
-        Movie m = mock(Movie.class);
-        when(m.getPriceCode())
-                .thenReturn(Regular);
-        when(m.getTitle())
-                .thenReturn("firstMovie");
+        Movie m = CreateMovie("firstMovie", Regular);
+        Movie m2 = CreateMovie("secondMovie", NewRelease);
+        Movie m3 = CreateMovie("thirdMovie", Childrens);
 
-        Rental r = new Rental(m, 1);
-
-        Movie m2 = mock(Movie.class);
-        when(m2.getPriceCode())
-                .thenReturn(NewRelease);
-        when(m2.getTitle())
-                .thenReturn("secondMovie");
-        Rental r2 = new Rental(m2, 1);
-
-        Movie m3 = mock(Movie.class);
-        when(m3.getPriceCode())
-                .thenReturn(Childrens);
-        when(m3.getTitle())
-                .thenReturn("firstMovie");
-        Rental r3 = new Rental(m3, 1);
+        Rental r = CreateRentalStub(m, 1);
+        Rental r2 = CreateRentalStub(m2, 2);
+        Rental r3 = CreateRentalStub(m3, 3);
 
         c.addRental(r);
         c.addRental(r2);
         c.addRental(r3);
 
-        Assert.assertThat(c.Statement(),  containsString("Amount owed is 6.5"));
-        Assert.assertThat(c.Statement(),  containsString("You earned 3 frequent renter points"));
+        Assert.assertThat(c.Statement(),  containsString("Amount owed is 9.5"));
+        Assert.assertThat(c.Statement(),  containsString("You earned 4 frequent renter points"));
 
         System.out.println(c.Statement());
     }
